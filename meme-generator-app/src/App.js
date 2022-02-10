114ca6798cea6af4d670f834a1f9e7a09bca9cb6
Alain Raimond <ark2784@gmail.com>
2022-02-10 15:04:20 -0600

    first commit

---
+++ meme-generator-app/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <img src="./public/head.png" alt="Italian Trulli"><img/>
+        <h2>Meme generator</h2>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
